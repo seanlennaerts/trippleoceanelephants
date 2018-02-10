cb83c958ca27dbffc0f43bfbaba11cebff5c2075
Janik Andreas <j.andreas@posteo.de>
2018-02-09 19:22:03 -0800

    Item query now works on the /search?term=<searchterm> endpoint.

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,35 @@ const aws_creds = JSON.parse(fs.readFileSync('./aws_creds.json', 'utf8'))	;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/search', (req, res) => {
-
-	var result = semanticsCall(req.query.term, res);
- });
- // scanning barcode
+  // console.log("Entering endpoint search, term: ")
+  client.itemSearch({
+    // director: 'Quentin Tarantino',
+    // actor: 'Samuel L. Jackson',
+    // searchIndex: 'DVD',
+    // audienceRating: 'R',
+    keywords: req.query.term,
+    responseGroup: 'ItemAttributes'
+  })
+  .then( (results) => {
+    // console.log("Looking items up through API...")
+    return Promise.all( results.map( (item) => {
+      return client.itemLookup({
+        ItemId: item.ASIN[0]
+      })
+    }))
+    // res = convert.xml2js(results, {ignoreComment: true})
+  })
+  .then( items => {
+    // console.log("Fetching all item results...")
+    return fetchItems(items);
+  })
+  .then( fetchedItems => {
+    // console.log("Fetched:\n" + fetchedItems)
+    res.json(fetchedItems)
+  })
+  .catch(err => { console.log(err)})
+});
+   // scanning barcode
 app.get('/barcode', (req, res) => {
   request( {uri:"https://api.upcitemdb.com/prod/trial/lookup?upc="+req.query.id})
   .then( (body) => {
@@ -24,8 +49,8 @@ app.get('/barcode', (req, res) => {
  	 var title = parsed.items[0].title;
 	 var response = semanticsCall(title, res);
  	 res.json(response);
-  });
  });
+});
 
 
  // 	debugger;
@@ -55,40 +80,74 @@ var client = amazon.createClient({
   locale: "ca"
 });
 
-client.itemSearch({
-  // director: 'Quentin Tarantino',
-  // actor: 'Samuel L. Jackson',
-  // searchIndex: 'DVD',
-  // audienceRating: 'R',
-  keywords: 'shampoo',
-  responseGroup: 'ItemAttributes'
-}).then( (results) => {
-	return Promise.all( results.map( (item) => {
-    return client.itemLookup({
-      ItemId: item.ASIN[0]
+function fetchItems(items) {
+return Promise.resolve(items).then( (results) => {
+  // debugger;
+  // ASINS = results.map( el => { return el[0].ASIN[0] });
+  return Promise.all( 
+    results.map( result => { 
+      var asin = result[0].ASIN[0];
+      var uri = `http://www.amazon.ca/product-reviews/${asin}/?ie=UTF8&showViewpoints=0&pageNumber=1&sortBy=bySubmissionDateAscending`;
+      return request( {uri: uri })
+        .then( (body) => {
+            $ = cheerio.load(body); 
+            var reviews = $('span[data-hook=review-body]').text();
+            return { ...result, reviews: { text: reviews, score: 0 }};
+        })
     })
-  }))
-  // res = convert.xml2js(results, {ignoreComment: true})
+  )
+  // debugger;
+  // descs = results.filter( el => { return el[0].EditorialReviews != undefined })
+  // descs = descs.map( el => { return el[0].EditorialReviews[0].EditorialReview[0].Content[0] })
+  // if (descs.length < 1) {
+  //   console.log("No items have editorial reviews...")
+  // }
+  // // results[1][0].EditorialReviews[0].EditorialReview[0].Content[0]
+  // descs = descs.map( (desc) => {
+  //   var hits = wordBank.reduce((total, element, index) => {
+  //     var hitsonword = 0;
+  //     var reg = new RegExp(element, "i");
+  //     hitsonword += desc.match(reg) ? 1 : 0;
+  //     return total + hitsonword; 
+  //   }, 0)
+  //   return hits
+  // })
+
+  // console.log(descs.join(" | "))
 })
-.then( (results) => {
-  debugger;
-  descs = results.filter( el => { return el[0].EditorialReviews != undefined })
-  descs = descs.map( el => { return el[0].EditorialReviews[0].EditorialReview[0].Content[0] })
-  if (descs.length < 1) {
-    console.log("No items have editorial reviews...")
-  }
-  // results[1][0].EditorialReviews[0].EditorialReview[0].Content[0]
-  descs = descs.map( (desc) => {
+.then( (items) => {
+ // var reviews = responses
+ //  .map( body => { 
+ //    $ = cheerio.load(body); 
+ //    return $('span[data-hook=review-body]').text();
+ //  });
+  // console.log(reviews);
+  for (item of items) {
     var hits = wordBank.reduce((total, element, index) => {
       var hitsonword = 0;
-      var reg = new RegExp(element, "i");
-      hitsonword += desc.match(reg) ? 1 : 0;
+      var reg = new RegExp(element);
+      hitsonword += item.reviews.text.match(reg) ? 1 : 0;
       return total + hitsonword; 
     }, 0)
-    return hits
-  })
-
-  console.log(descs.join(" | "))
+    item.reviews.score = hits;
+  }
+  // var  = reviews.map( (productReviews) => {
+  //   var hits = wordBank.reduce((total, element, index) => {
+  //     var hitsonword = 0;
+  //     var reg = new RegExp(element);
+  //     hitsonword += productReviews.match(reg) ? 1 : 0;
+  //     return total + hitsonword; 
+  //   }, 0)
+  //   return { hits
+  // })
+  // console.log(JSON.stringify(items));
+  items.sort( (item1, item2) => {
+    var val = item2.reviews.score -item1.reviews.score ;
+    return  val / Math.abs(val);
+  });
+  console.log(items.map( item => { return item.reviews.score; }).join(" | "));
+  return items;
+  // fs.writeFile('./log.txt', JSON.stringify(reviews), (err) => { console.log(err) })
 })
 .catch(function(err){
 	// var err_obj = JSON.parse(err);
@@ -96,7 +155,7 @@ client.itemSearch({
   debugger;
    console.log(err);
 });
-
+}
 // client.itemLookup({
 //   ItemId: "B005GNU60U"
 // }).then(function(results){
@@ -109,7 +168,7 @@ client.itemSearch({
 //    console.log(err);
 // });
 
-const wordBank = ['eco', 'sustainable', 'eco-friendly', 'sustainability', 'environment'];
+const wordBank = ['eco', 'sustainable', 'eco-friendly', 'sustainability', 'environment','i','really' ];
 const amazonSelectors = ["#feature-bullets ul li span", "#productDescription p"];
 const walmartSelectors = [".product-about .about-desc"];
 const targetSelectors = ["#tab-content-details div"];
